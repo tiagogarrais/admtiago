6f9af7a5abeaa32be58e01c118fc63e551a053a0
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-11-04 15:33:53 -0300

    Atualizar o solicita.jsx
    Novas solicitações serão feitas no Sistema próprio da Universidade Federal do Cariri.

--- pages/transportes/solicita.jsx
+++ pages/transportes/solicita.jsx
@@ -2,5 +2,5 @@ import SolicitarVeiculo from "../../components/transportes/SolicitarVeiculo";
 import SolicitarVeiculoGreve from "../../components/transportes/SolicitarVeiculoGreve";
 
 export default function Solicita() {
-  return <SolicitarVeiculo />;
+  return <p>Novas solicitaçõesdevem ser feitas no SIPAC</p>;
 }
