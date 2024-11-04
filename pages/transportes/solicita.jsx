938366b2ca2f3bba2ded9c4d0a5b677b9fd8347f
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-11-04 15:40:12 -0300

    Atualizar o solicita.jsx

--- pages/transportes/solicita.jsx
+++ pages/transportes/solicita.jsx
@@ -2,5 +2,5 @@ import SolicitarVeiculo from "../../components/transportes/SolicitarVeiculo";
 import SolicitarVeiculoGreve from "../../components/transportes/SolicitarVeiculoGreve";
 
 export default function Solicita() {
-  return <p>Novas solicitaçõesdevem ser feitas no SIPAC</p>;
+  return <p>À partir de 04/11/2024, novas solicitações devem ser feitas diretamente no SIPAC</p>;
 }
