9b78ca5c0e65f8ce25615f6d5a3527b295971ac1
computador <tiagoarraisholanda@gmail.com>
2025-01-01 23:47:03 -0300

    Apagando tudo que não é utilizado

--- pages/api/brainstorming/buscarIdeias.jsx
+++
@@ -1,8 +0,0 @@
-import connect from "../../../src/utils/mongodb"
-
-export default async function buscarIdeias(require, response) {
-    const { db } = await connect()
-    const ideias = await db.collection('brainstorming').find().sort({endereco:1}).toArray()
-    response.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate')
-    response.status(200).json(ideias)
-}
--- pages/transportes/solicita.jsx
+++
@@ -1,6 +0,0 @@
-import SolicitarVeiculo from "../../components/transportes/SolicitarVeiculo";
-import SolicitarVeiculoGreve from "../../components/transportes/SolicitarVeiculoGreve";
-
-export default function Solicita() {
-  return <p>À partir de 04/11/2024, novas solicitações devem ser feitas diretamente no SIPAC</p>;
-}
