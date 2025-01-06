7da8a02c7dc866d1271ac433ca9325b60010e638
computador <tiagoarraisholanda@gmail.com>
2025-01-06 02:43:30 -0300

    Adicionando autenticação e banco de dados.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'raiolaser.16mb.com',
-        port: ''
-      }
-    ]
-  }
-}
+};
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,7 @@ import Rodape from "../components/Rodape";
 import NextNProgress from "nextjs-progressbar";
 import "../styles/globals.css";
 import Head from "next/head";
+import { SessionProvider } from "next-auth/react";
 
 export default function MyApp({
   Component,
@@ -10,26 +11,28 @@ export default function MyApp({
 }) {
   return (
     <>
-      <Head>
-        <title>admtiago.com.br</title>
-        <meta
-          name="Site de serviços do Adm. Tiago das Graças Arrais CRA 11660"
-          content="Serviços digitais"
-          charset="utf-8"
-          lang="pt-BR"
+      <SessionProvider session={session}>
+        <Head>
+          <title>admtiago.com.br</title>
+          <meta
+            name="Site de serviços do Adm. Tiago das Graças Arrais CRA 11660"
+            content="Serviços digitais"
+            charset="utf-8"
+            lang="pt-BR"
+          />
+        </Head>
+        <Cabecalho />
+        <hr />
+        <NextNProgress
+          color="#29D"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={5}
+          showOnShallow={true}
         />
-      </Head>
-      <Cabecalho />
-      <hr />
-      <NextNProgress
-        color="#29D"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={5}
-        showOnShallow={true}
-      />
-      <Component {...pageProps} />
-      <Rodape />
+        <Component {...pageProps} />
+        <Rodape />
+      </SessionProvider>
     </>
   );
 }
