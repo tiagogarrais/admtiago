40778ffda1861f6d46c55dd582de3bc0a0f757e9
tiagogarrais <tiagoarraisholanda@gmail.com>
2025-01-24 11:37:28 -0300

    Esta versão funciona com PostgreSQL e Docker

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,19 @@
 export default function Index() {
   return (
-    <div className="center">
-      <button>
-        <a href="https://conexaofuturo.com.br">https://conexaofuturo.com.br</a>
-      </button>
-      <button>
-        <a href="https://sitedotiago.com.br">https://sitedotiago.com.br</a>
-      </button>
-    </div>
+    <>
+      <div className="center">
+        <button>
+          <a href="https://conexaofuturo.com.br">
+            https://conexaofuturo.com.br
+          </a>
+        </button>
+      </div>
+
+      <div className="center">
+        <button>
+          <a href="https://sitedotiago.com.br">https://sitedotiago.com.br</a>
+        </button>
+      </div>
+    </>
   );
 }
