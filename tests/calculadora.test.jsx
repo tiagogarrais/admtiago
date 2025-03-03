75ee3db52d33eba5e1c7e500cd5c412daeeb5545
computador <tiagoarraisholanda@gmail.com>
2025-03-03 12:08:12 -0300

    Incluindo o model `cacluladora` e um `test` para testar a funcionalidade de testes.

---
+++ tests/calculadora.test.jsx
@@ -0,0 +1,6 @@
+const calculadora = require("../models/calculadora.jsx");
+
+test("Somar 2 + 2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
