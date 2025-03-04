91e704796b8451037009be892035c9844d2b1f85
computador <tiagoarraisholanda@gmail.com>
2025-03-04 11:27:37 -0300

    Adicionando funções e testes na calculadora.

--- models/calculadora.jsx
+++ models/calculadora.jsx
@@ -2,4 +2,19 @@ function somar(num1, num2) {
   return num1 + num2;
 }
 
+function subtrair(num1, num2) {
+  return num1 - num2;
+}
+
+function dividir(num1, num2) {
+  return num1 / num2;
+}
+
+function multiplicar(num1, num2) {
+  return num1 * num2;
+}
+
 exports.somar = somar;
+exports.subtrair = subtrair;
+exports.dividir = dividir;
+exports.multiplicar = multiplicar;
--- tests/calculadora.test.jsx
+++ tests/calculadora.test.jsx
@@ -4,3 +4,18 @@ test("Somar 2 + 2 deveria retornar 4", () => {
   const resultado = calculadora.somar(2, 2);
   expect(resultado).toBe(4);
 });
+
+test("Subtrair 9 - 5 deveria retornar 4", () => {
+  const resultado = calculadora.subtrair(9, 5);
+  expect(resultado).toBe(4);
+});
+
+test("Dividir 10 / 5 deveria retornar 2", () => {
+  const resultado = calculadora.dividir(10, 5);
+  expect(resultado).toBe(2);
+});
+
+test("Multiplicar 45 * 7 deveria retornar 315", () => {
+  const resultado = calculadora.multiplicar(45, 7);
+  expect(resultado).toBe(315);
+});
